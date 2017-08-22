f5d71dc10575dff1bdcf882c563473c29be1649a
Anton Domashnev <antondomashnev@gmail.com>
2017-08-22 17:00:02 +0200

    Initial Commit

---
+++ SPMIssueReport/SPMIssueReport.h
@@ -0,0 +1,19 @@
+//
+//  SPMIssueReport.h
+//  SPMIssueReport
+//
+//  Created by Anton Domashnev on 22.08.17.
+//  Copyright © 2017 Antondomashnev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SPMIssueReport.
+FOUNDATION_EXPORT double SPMIssueReportVersionNumber;
+
+//! Project version string for SPMIssueReport.
+FOUNDATION_EXPORT const unsigned char SPMIssueReportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPMIssueReport/PublicHeader.h>
+
+
